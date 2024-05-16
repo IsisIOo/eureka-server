fb6631852c85d736457bcce99f49544c54851c8d
Isidora <isidora.oyanedel@usach.cl>
2024-05-16 00:11:39 -0400

    no ejecuta

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -2,13 +2,14 @@ package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
